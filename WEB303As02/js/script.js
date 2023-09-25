c8c5e9da8a5319eebfe325dd3891adafba8192c3
deepannsu <122818510+deepannsu@users.noreply.github.com>
2023-09-24 22:21:54 -0400

    update the script.js
    animate the div#content

--- WEB303As02/js/script.js
+++ WEB303As02/js/script.js
@@ -1,24 +1,34 @@
 $(document).ready(function() {
-    // Function to load content using AJAX
-    function loadContent(url) {
-        // Use AJAX to load content from the specified URL
-        $.ajax({
-            url: url,
-            method: 'GET',
-            success: function(data) {
-                // Set the loaded content into the #content div
-                $('#content').html(data);
-            },
-            error: function() {
-                console.log('Error loading content.');
-            }
+    // Function to load content using AJAX and animate
+    function loadAndAnimateContent(url) {
+        // Hide content with animation
+        $('#content').fadeOut('fast', function() {
+            // Clear the content
+            $(this).empty();
+
+            // Load new content using AJAX
+            $.ajax({
+                url: url,
+                method: 'GET',
+                success: function(data) {
+                    // Add the loaded content to #content
+                    $('#content').html(data);
+
+                    // Animate the content with fadeIn
+                    $('#content').fadeIn('fast');
+                },
+                error: function() {
+                    console.log('Error loading content.');
+                }
+            });
         });
     }
 
     // Event handler for link clicks
     $('.load-content').click(function(e) {
-        e.preventDefault(); // Prevent the default link behavior (navigation)
-        var contentUrl = $(this).attr('href'); // Get the URL from the link's href attribute
-        loadContent(contentUrl); // Load the content using AJAX
+        e.preventDefault();
+        var contentUrl = $(this).attr('href');
+        loadAndAnimateContent(contentUrl);
     });
 });
+
