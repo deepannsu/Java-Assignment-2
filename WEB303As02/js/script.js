cc0f98da6e730720d1ebea1f77a75c50f19d23f6
deepannsu <122818510+deepannsu@users.noreply.github.com>
2023-09-24 22:25:03 -0400

    update script.js
    hide div#content

--- WEB303As02/js/script.js
+++ WEB303As02/js/script.js
@@ -28,7 +28,50 @@ $(document).ready(function() {
     $('.load-content').click(function(e) {
         e.preventDefault();
         var contentUrl = $(this).attr('href');
-        loadAndAnimateContent(contentUrl);
+        
+        // Check if the clicked link is different from the current content
+        if (contentUrl !== $('#content').data('current-url')) {
+            loadAndAnimateContent(contentUrl);
+            $('#content').data('current-url', contentUrl); // Store the current URL
+        }
+    });
+});
+$(document).ready(function() {
+    // Function to load content using AJAX and animate
+    function loadAndAnimateContent(url) {
+        // Hide content with animation
+        $('#content').fadeOut('fast', function() {
+            // Clear the content
+            $(this).empty();
+
+            // Load new content using AJAX
+            $.ajax({
+                url: url,
+                method: 'GET',
+                success: function(data) {
+                    // Add the loaded content to #content
+                    $('#content').html(data);
+
+                    // Animate the content with fadeIn
+                    $('#content').fadeIn('fast');
+                },
+                error: function() {
+                    console.log('Error loading content.');
+                }
+            });
+        });
+    }
+
+    // Event handler for link clicks
+    $('.load-content').click(function(e) {
+        e.preventDefault();
+        var contentUrl = $(this).attr('href');
+        
+        // Check if the clicked link is different from the current content
+        if (contentUrl !== $('#content').data('current-url')) {
+            loadAndAnimateContent(contentUrl);
+            $('#content').data('current-url', contentUrl); // Store the current URL
+        }
     });
 });
 
